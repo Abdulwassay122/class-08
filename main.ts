871ae7ed69eb2cbee34736abac88523318745b8a
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-04-29 08:48:52 -0700

    first

---
+++ main.ts
@@ -0,0 +1,51 @@
+// Logic statements
+
+// let age = 89
+// if(age < 18){
+//     console.log("you can't drive")
+// }else{
+//     console.log("you  can drive")
+// }
+
+// let per = 33
+// if(per > 80){
+//     console.log("Your grade is A1")
+// }
+// else if(per > 70){
+//     console.log("Your grade is A")
+// }
+// else if(per > 60){
+//     console.log("Your grade is B")
+// }
+// else if(per > 50){
+//     console.log("Your grade is C")
+// }
+// else if(per > 33){
+//     console.log("Your grade is D")
+// }
+// else if(per < 33){
+//     console.log("Yoyu are Fail")
+// }
+
+let totalmarks = 850
+let obtainedmarks = 640
+let per = obtainedmarks/totalmarks *100
+
+if(per > 80){
+    console.log("Your grade is A1")
+}
+else if(per > 70){
+    console.log("Your grade is A")
+}
+else if(per > 60){
+    console.log("Your grade is B")
+}
+else if(per > 50){
+    console.log("Your grade is C")
+}
+else if(per > 33){
+    console.log("Your grade is D")
+}
+else if(per < 33){
+    console.log("Yoyu are Fail")
+}
